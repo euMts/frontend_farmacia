ccc1d5f2ab744293a1a4cc7ec308e3e40a58e490
Matheus Eduardo <matheusetp@hotmail.com>
2023-09-29 20:49:20 -0300

    updated files

--- src/components/panoramaChart/CustomTooltip.js
+++ src/components/panoramaChart/CustomTooltip.js
@@ -1,6 +1,6 @@
 function CustomTooltip({ payload }) {
   const { payload: internalPayload } = payload[1] || {};
-  console.log(internalPayload?.original);
+  // console.log(internalPayload?.original);
   return (
     <div
       className="custom-tooltip"
@@ -10,12 +10,42 @@ function CustomTooltip({ payload }) {
         // width: "300px",
         // height: "500px",
         justifyContent: "space-around",
-        backgroundColor: "white"
+        backgroundColor: "white",
+        border: "1px solid #A9A9A9",
+        borderRadius: "4px",
+        // boxShadow: "10px 10px 20px black",
+        padding: "4px"
       }}
     >
       {Object.entries(internalPayload?.original || {}).map(([key, value]) => (
-        <div style={{ padding: "15px" }}>
-          {key} -{value}
+        <div style={{ padding: "8px", fontSize: "12px" }}>
+          {key === "month" ? (
+            <p id="month-name" style={{ fontSize: "15px", fontWeight: "bold" }}>
+              {value}
+            </p>
+          ) : (
+            <div
+              style={{
+                display: "flex",
+                flexDirection: "row",
+                justifyContent: "left",
+                alignItems: "center",
+              }}
+            >
+              <div
+                style={{
+                  width: "8px",
+                  height: "8px",
+                  backgroundColor: "red",
+                  marginRight: "5px",
+                }}
+                id="square-color"
+              ></div>
+              <div id="product-name-info">
+                {key}: {value}
+              </div>
+            </div>
+          )}
         </div>
       ))}
     </div>
